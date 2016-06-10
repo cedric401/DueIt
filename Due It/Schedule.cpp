9e71cd53026d64491b5379f11e0474da02f5b6fc
t-hagerty <thagerty_0213@email.ric.edu>
2016-06-10 00:47:24 -0400

    Finished Method Headers
    -Typed out the method headers from the UML model for the Schedule class.

--- Due It/Schedule.cpp
+++ Due It/Schedule.cpp
@@ -1,14 +1,51 @@
 #include "Schedule.h"
 
-
 namespace DueItModel
 {
 	Schedule::Schedule()
 	{
 	}
 
-
 	Schedule::~Schedule()
+	{
+	}
+	void Schedule::updateTime()
+	{
+	}
+	void Schedule::addTask(Task newTask)
+	{
+	}
+	void Schedule::deleteTask(Task aTask)
+	{
+	}
+	void Schedule::deleteAllTasksForCourse(int courseNumber)
+	{
+	}
+	void Schedule::deleteAllTasksForJob(std::string companyName)
+	{
+	}
+	void Schedule::deleteAllTimePeriod(int strtT, int strtD, int strtM, int strtY, int endT, int endD, int endM, int endY)
+	{
+	}
+	void Schedule::changeScheduleTimePeriod(int strtT, int strtD, int strtM, int strtY, int endT, int endD, int endM, int endY)
+	{
+	}
+	void Schedule::setTypeTasksShown(int type)
+	{
+	}
+	std::vector<Task> Schedule::findStudyTime()
+	{
+		return std::vector<Task>();
+	}
+	std::string Schedule::toString()
+	{
+		return std::string();
+	}
+	Task Schedule::getTask(int index)
+	{
+		return Task();
+	}
+	void Schedule::sortTasks()
 	{
 	}
 };
--- Due It/Schedule.h
+++ Due It/Schedule.h
@@ -8,9 +8,25 @@ namespace DueItModel
 	public:
 		Schedule();
 		~Schedule();
+		void updateTime();
+		void addTask(Task newTask);
+		void deleteTask(Task aTask);
+		void deleteAllTasksForCourse(int courseNumber);
+		void deleteAllTasksForJob(std::string companyName);
+		void deleteAllTimePeriod(int strtT, int strtD, int strtM, int strtY, int endT, int endD, int endM, int endY);
+		void changeScheduleTimePeriod(int strtT, int strtD, int strtM, int strtY, int endT, int endD, int endM, int endY);
+		void setTypeTasksShown(int type);
+		std::vector<Task> findStudyTime();
+		std::string toString();
+		Task getTask(int index);
 
 	private:
 		std::vector<Task> currentSchedule;
+		int currentTime; //0 <= startTime <= 86,399 (86,400 seconds in a day)
+		int currentDay; //1 <= day <= 31
+		int currentMonth; //1 <= month <= 12
+		int currentYear;
+		void sortTasks();
 	};
 };
 
